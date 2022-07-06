0ade0e3f1bf835f5982b60a469874610ca378af0
Rafael <108157205+MouseDataScientist@users.noreply.github.com>
2022-07-06 12:39:34 -0300

    Add files via upload

--- idh_project/streamlit_app.py
+++ idh_project/streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit as st
 import numpy as np
 import os
 import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
 
 st.header('Previsor de IDH')
-st.markdown('Configure os parâmetros abaixo para efetuar a previsão')
+st.markdown('1º Configure os parâmetros abaixo')
+st.markdown('2º Clieque em "PREVER" para efetuar a previsão')
 
 ile = st.slider('Informe o Índice de Liberdade Econômica', min_value = 0.00, max_value = 10.00)
 st.write(ile)
@@ -12,17 +14,39 @@ st.write(ile)
 ipc = st.slider('Informe o Índice de Percepção da Corrupção', min_value = 0, max_value = 100)
 st.write(ipc)
 
-if (os.path.exists('idh_project/modelo.h5')):
-    modelo = tf.keras.models.load_model('idh_project/modelo.h5')
+def prever(x):
+    previsao = modelo.predict(x)
+    if previsao[:,0] > 0.50:
+        st.write('O IDH tende a ser ALTO!')
+    elif previsao[:,1] > 0.50:
+        st.write('O IDH tende a ser BAIXO!')
+    else:
+        st.write('O IDH tende a ser MÉDIO!')
+        
+normalizar = MinMaxScaler(feature_range = (0,1))
+
+if ((os.path.exists('modelo.h5')) and (os.path.exists('previsores.npy')) and (os.path.exists('normalizados.npy'))):
+    modelo = tf.keras.models.load_model('modelo.h5')
+    previsores = np.load('previsores.npy')
+    normalizados = np.load('normalizados.npy')
     botao = st.button('PREVER')
+    
     if (botao):
-        listaValores = np.array([[ile, ipc]])
-        previsao = modelo.predict(listaValores)
-        if previsao[:, 0] >= 0.50:
-            st.write('O IDH tende a ser ALTO!')
-        elif previsao[:, 1] >=0.50:
-            st.write('O IDH tende a ser BAIXO!')
-        else:
-            st.write('O IDH tende a ser MÉDIO!')
+        for i in range(len(previsores)):
+            cont = i + 1
+            novo = [[ile, ipc]]
+            
+            if np.all(novo[0] == previsores[i]):
+                x = np.array([normalizados[i]])
+                prever(x)
+                break
+    
+            else:
+                cont == len(previsores)
+                previsores = np.append(previsores, novo, axis = 0)
+                normalizados = normalizar.fit_transform(previsores)
+                x = np.array([normalizados[-1]])
+                prever(x)
+                break
 else:
-    st.error('Erro ao carregar modelo preditivo, contate o administrador do sistema!')
+    st.error('Erro ao carregar arquivos, contate o administrador do sistema!')
